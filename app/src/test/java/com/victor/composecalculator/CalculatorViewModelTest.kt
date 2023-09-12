8dadf8b10d4cdc827bdefe2b35c90960457e3826
victor-medeiros <victormedeirosms@gmail.com>
2023-09-12 02:01:47 -0300

    :wip: Adjusting test

--- app/src/test/java/com/victor/composecalculator/CalculatorViewModelTest.kt
+++ app/src/test/java/com/victor/composecalculator/CalculatorViewModelTest.kt
@@ -1,21 +1,39 @@
 package com.victor.composecalculator
 
 import com.victor.composecalculator.model.UiEvent
-import io.mockk.mockk
-import org.junit.Assert.assertTrue
+import org.junit.Before
 import org.junit.Test
 
 
 class CalculatorViewModelTest {
 
-    val viewModel = mockk<CalculatorViewModel>()
+    private lateinit var viewModel: CalculatorViewModel
+
+    @Before
+    fun setup() {
+        viewModel = CalculatorViewModel()
+    }
 
     @Test
-    fun `Given user type a number, then currentNumber should be set`() {
+    fun `Given user type a number, then currentNumber should be set to it`() {
         for (i in 1..10) {
-            val num = (1..1000).random()
+            val num = (0..9).random()
             viewModel.onEvent(UiEvent.TypeNumber(num))
-            assertTrue(viewModel.currentNumber.value == num)
+            assert(viewModel.currentNumber.value == num.toString())
+        }
+    }
+
+    @Test
+    fun `Given a user type many numbers, the currentNumber should concatenate them` () {
+        for (i in 1..5) {
+            val n = (1..5).random()
+            var currentNumber = ""
+            for (j in 0 until n) {
+                val num = (0..9).random()
+                viewModel.onEvent(UiEvent.TypeNumber(num))
+                currentNumber += num
+            }
+            assert(viewModel.currentNumber.value == currentNumber)
         }
     }
 }
