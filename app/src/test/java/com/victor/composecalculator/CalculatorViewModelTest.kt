b071293846958cd3b3a09491f41cf05fa157c35b
victor-medeiros <victormedeirosms@gmail.com>
2023-09-12 11:58:24 -0300

    :white_check_mark: Adjusting test  and adding feature of typing a number

--- app/src/test/java/com/victor/composecalculator/CalculatorViewModelTest.kt
+++ app/src/test/java/com/victor/composecalculator/CalculatorViewModelTest.kt
@@ -1,5 +1,6 @@
 package com.victor.composecalculator
 
+import com.google.common.truth.Truth.assertThat
 import com.victor.composecalculator.model.UiEvent
 import org.junit.Before
 import org.junit.Test
@@ -17,15 +18,17 @@ class CalculatorViewModelTest {
     @Test
     fun `Given user type a number, then currentNumber should be set to it`() {
         for (i in 1..10) {
+            viewModel = CalculatorViewModel()
             val num = (0..9).random()
             viewModel.onEvent(UiEvent.TypeNumber(num))
-            assert(viewModel.currentNumber.value == num.toString())
+            assertThat(viewModel.currentNumber.value).isEqualTo(num.toString())
         }
     }
 
     @Test
     fun `Given a user type many numbers, the currentNumber should concatenate them` () {
         for (i in 1..5) {
+            viewModel = CalculatorViewModel()
             val n = (1..5).random()
             var currentNumber = ""
             for (j in 0 until n) {
@@ -33,7 +36,7 @@ class CalculatorViewModelTest {
                 viewModel.onEvent(UiEvent.TypeNumber(num))
                 currentNumber += num
             }
-            assert(viewModel.currentNumber.value == currentNumber)
+            assertThat(viewModel.currentNumber.value).isEqualTo(currentNumber)
         }
     }
 }
